df898e687603c229c22fb7030eeaba525d459bb4
sonkar-amit <sonkar.amit08@gmail.com>
2016-12-09 20:41:58 +0530

    JS Assignment3

---
+++ Assignment3/app.js
@@ -0,0 +1,15 @@
+var employee = {"name":"Amit",
+                "age":"27",
+                "salary":1500000,
+                "address":{"city":"Bangalore",
+                           "state":"Karnataka",
+                           "pincode":"560043"
+                          }
+               };
+
+console.log(employee.name);
+console.log(employee.age);
+console.log(employee.salary);
+console.log(employee.address.city);
+console.log(employee.address.state);
+console.log(employee.address.pincode);
